eb67a162674f4dbe7633f93f183686035e1a3772
sionaris <aressionakides@gmail.com>
2023-09-17 20:38:01 +0100

    Fixed single sample prediction issues

--- server.R
+++ server.R
@@ -1411,10 +1411,11 @@ server <- function(input, output, session) {
                              random_treatment_vector, random_iC10_vector,
                              random_mammaprint_vector, random_rorS_vector,
                              random_scmod1_vector)
-      names(full_random_vector) = colnames(full_ml_set)[3:189]
       
       # Make reactive
-      imported_data(as.data.frame(full_random_vector))
+      full_random_vector = as.data.frame(t(full_random_vector))
+      colnames(full_random_vector) = colnames(full_ml_set)[3:189]
+      imported_data(full_random_vector)
       
       # Verification message
       showModal(
@@ -1743,10 +1744,20 @@ server <- function(input, output, session) {
     # Convert to reactive
     imported_data(as.data.frame(input_data))
     
+    # If all those checks passed and X_handle == "off", convert the Entrez ID's
+    # to X_Entrez format
+    if (X_handle == "off") {
+      current_data = imported_data()
+      cols_to_rename <- gene_columns_without_X
+      new_colnames <- gene_columns_with_X
+      colnames(current_data)[which(names(current_data) %in% cols_to_rename)] <- new_colnames
+      imported_data(current_data)
+    }
+    
     # Treatment
     treatment_toggle = reactive({ input$breast_cancer_new_prediction_prespecified_treatment })
     observeEvent(treatment_toggle(), {
-      if (treatment_toggle() == "No") {
+      if (!is.null(imported_data()) && treatment_toggle() == "No") {
         shinyjs::enable("breast_cancer_new_prediction_treatment")
         desired_treatment = reactive({ input$breast_cancer_new_prediction_treatment })
         
@@ -1771,31 +1782,32 @@ server <- function(input, output, session) {
           current_data$Endo = factor(current_data$Endo, levels = c(0, 1), labels = c(0, 1))
           imported_data(current_data)
         }
-      } else {
+      } else if(!is.null(imported_data()) && treatment_toggle() == "Yes")  {
         shinyjs::disable("breast_cancer_new_prediction_treatment")
         current_data = imported_data()
         current_data$Endo = factor(current_data$Endo, levels = c(0, 1), labels = c(0, 1))
         imported_data(current_data)
       }
     })
+    print("We passed treatment")
   })
-  
+  print("We passed the first check")
+
   # Sample pheno editing:
   # pam50
   pam50_subtype_edit = reactive({ input$breast_cancer_new_prediction_pam50_annotation })
   observeEvent(pam50_subtype_edit(), {
+    # Create intermediate variable
+    current_data = imported_data()
     if (!is.null(imported_data()) && pam50_subtype_edit() != "Preset") {
       if (pam50_subtype_edit() == "Random") {
-        pam50s = c("Luminal A", "Luminal B", "Normal-like", "Basal-like", "HER2+")
-        new_subtype = pam50s[sample(seq(1, 5), 1)]
-        pam50_subtype_edit(new_subtype)  # Update the reactive expression
-      }
-      
-      # Create intermediate variable
-      current_data = imported_data()
-
-      # Edit data
-      if (pam50_subtype_edit() == "Luminal A") {
+        pam50s = c(`Luminal A` = c(0, 0, 1, 0), 
+                   `Luminal B` = c(0, 1, 0, 0), 
+                   `Normal-like` = c(0, 0, 0, 1), 
+                   `Basal-like`= c(1, 0, 0, 0), 
+                   `HER2+` = c(0, 0, 0, 0))
+        current_data[, c("HER2", "LumB", "LumA", "Normal")] = pam50s[[sample(seq(1, 5), 1)]]
+      } else if (pam50_subtype_edit() == "Luminal A") {
         current_data[, c("HER2", "LumB", "LumA", "Normal")] = c(0, 0, 1, 0)
       } else if (pam50_subtype_edit() == "Luminal B") {
         current_data[, c("HER2", "LumB", "LumA", "Normal")] = c(0, 1, 0, 0)
@@ -1806,8 +1818,18 @@ server <- function(input, output, session) {
       } else if (pam50_subtype_edit() == "Basal-like") {
         current_data[, c("HER2", "LumB", "LumA", "Normal")] = c(0, 0, 0, 0)
       }
-      
       # Update imported_data()
+      current_data[, c("HER2", "LumB", "LumA", "Normal")] <- 
+        lapply(current_data[, c("HER2", "LumB", "LumA", "Normal")], function(x) 
+          factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
+      imported_data(current_data)
+    } else if (!is.null(imported_data()) && pam50_subtype_edit() == "Preset") {
+      # Update imported_data()
+      current_data[, c("HER2", "LumB", "LumA", "Normal")] <- 
+        lapply(current_data[, c("HER2", "LumB", "LumA", "Normal")], function(x) 
+          factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
       imported_data(current_data)
     }
   })
@@ -1815,19 +1837,16 @@ server <- function(input, output, session) {
   # scmod1
   scmod1_subtype_edit = reactive({ input$breast_cancer_new_prediction_scmod1_annotation })
   observeEvent(scmod1_subtype_edit(), {
+    # Create intermediate variable
+    current_data = imported_data()
     if (!is.null(imported_data()) && scmod1_subtype_edit() != "Preset") {
       if (scmod1_subtype_edit() == "Random") {
-        scmod1s = c("ER-/HER2-", "ER+/HER2- high proliferation",
-                    "ER+/HER2- low proliferation", "HER2+")
-        new_subtype = scmod1s[sample(seq(1, 4), 1)]
-        scmod1_subtype_edit(new_subtype)  # Update the reactive expression
-      }
-      
-      # Create intermediate variable
-      current_data = imported_data()
-      
-      # Edit data
-      if (scmod1_subtype_edit() == "ER-/HER2-") {
+        scmod1s = c(`ER-/HER2-` = c(0, 0, 0), 
+                    `ER+/HER2- high proliferation` = c(1, 0, 0),
+                    `ER+/HER2- low proliferation` = c(0, 1, 0), 
+                    `HER2+` = c(0, 0, 1))
+        current_data[, c("ER_hp", "ER_lp", "HER2_scmod1")] = scmod1s[[sample(seq(1, 4), 1)]]
+      } else if (scmod1_subtype_edit() == "ER-/HER2-") {
         current_data[, c("ER_hp", "ER_lp", "HER2_scmod1")] = c(0, 0, 0)
       } else if (scmod1_subtype_edit() == "ER+/HER2- high proliferation") {
         current_data[, c("ER_hp", "ER_lp", "HER2_scmod1")] = c(1, 0, 0)
@@ -1836,8 +1855,18 @@ server <- function(input, output, session) {
       } else if (scmod1_subtype_edit() == "HER2+") {
         current_data[, c("ER_hp", "ER_lp", "HER2_scmod1")] = c(0, 0, 1)
       }
-      
       # Update imported_data()
+      current_data[, c("ER_hp", "ER_lp", "HER2_scmod1")] <- 
+        lapply(current_data[, c("ER_hp", "ER_lp", "HER2_scmod1")], function(x) 
+          factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
+      imported_data(current_data)
+    } else if (!is.null(imported_data()) && smod1_subtype_edit() == "Preset") {
+      # Update imported_data()
+      current_data[, c("ER_hp", "ER_lp", "HER2_scmod1")] <- 
+        lapply(current_data[, c("ER_hp", "ER_lp", "HER2_scmod1")], function(x) 
+          factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
       imported_data(current_data)
     }
   })
@@ -1845,24 +1874,29 @@ server <- function(input, output, session) {
   # Timepoint
   timepoint_edit = reactive({ input$breast_cancer_new_prediction_timepoint_annotation })
   observeEvent(timepoint_edit(), {
+    # Create intermediate variable
+    current_data = imported_data()
     if (!is.null(imported_data()) && timepoint_edit() != "Preset") {
       if (timepoint_edit() == "Random") {
-        timepoints = c("Pre-treatment", "On-treatment")
-        new_timepoint = timepoints[sample(seq(1, 2), 1)]
-        timepoint_edit(new_timepoint)
-      }
-      
-      # Create intermediate variable
-      current_data = imported_data()
-      
-      # Edit data
-      if (timepoint_edit() == "Pre-treatment") {
+        timepoints = c(`Pre-treatment` = 0, `On-treatment` = 1)
+        current_data[, "T2"] = timepoints[[sample(seq(1, 2), 1)]]
+      } else if (timepoint_edit() == "Pre-treatment") {
         current_data[, "T2"] = 0
       } else {
         current_data[, "T2"] = 1
       }
-      
       # Update imported_data()
+      current_data[, "T2"] <- 
+        lapply(current_data[, "T2"], function(x) 
+          factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
+      imported_data(current_data)
+    } else if (!is.null(imported_data()) && timepoint_edit() == "Preset") {
+      # Update imported_data()
+      current_data[, "T2"] <- 
+        lapply(current_data[, "T2"], function(x) 
+          factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
       imported_data(current_data)
     }
   })
@@ -1870,20 +1904,24 @@ server <- function(input, output, session) {
   # iC10
   iC10_edit = reactive({ input$breast_cancer_new_prediction_ic10_annotation })
   observeEvent(iC10_edit(), {
+    # Create intermediate variable
+    current_data = imported_data()
     if (!is.null(imported_data()) && iC10_edit() != "Preset") {
       if (iC10_edit() == "Random") {
-        iC10s = c("iC1", "iC2", "iC3", "iC4",
-                  "iC5", "iC6", "iC7", "iC8", "iC9",
-                  "iC10")
-        new_iC10 = iC10s[sample(seq(1, 10), 1)]
-        iC10_edit(new_iC10)
-      }
-      
-      # Create intermediate variable
-      current_data = imported_data()
-      
-      # Edit data
-      if (iC10_edit() == "iC1") {
+        iC10s = c(`iC1` = c(0, 0, 0, 0, 0, 0, 0, 0, 0), 
+                  `iC2` = c(1, 0, 0, 0, 0, 0, 0, 0, 0),
+                  `iC3` = c(0, 1, 0, 0, 0, 0, 0, 0, 0), 
+                  `iC4` = c(0, 0, 1, 0, 0, 0, 0, 0, 0),
+                  `iC5` = c(0, 0, 0, 1, 0, 0, 0, 0, 0), 
+                  `iC6` = c(0, 0, 0, 0, 1, 0, 0, 0, 0), 
+                  `iC7` = c(0, 0, 0, 0, 0, 1, 0, 0, 0), 
+                  `iC8` = c(0, 0, 0, 0, 0, 0, 1, 0, 0), 
+                  `iC9` = c(0, 0, 0, 0, 0, 0, 0, 1, 0),
+                  `iC10` = c(0, 0, 0, 0, 0, 0, 0, 0, 1))
+        current_data[, c("IC2", "IC3", "IC4",
+                         "IC5", "IC6", "IC7", "IC8", "IC9",
+                         "IC10")] = iC10s[[sample(seq(1, 10), 1)]]
+      } else if (iC10_edit() == "iC1") {
         current_data[, c("IC2", "IC3", "IC4",
                          "IC5", "IC6", "IC7", "IC8", "IC9",
                          "IC10")] = c(0, 0, 0, 0, 0, 0, 0, 0, 0)
@@ -1924,8 +1962,26 @@ server <- function(input, output, session) {
                          "IC5", "IC6", "IC7", "IC8", "IC9",
                          "IC10")] = c(0, 0, 0, 0, 0, 0, 0, 0, 1)
       }
-      
       # Update imported_data()
+      current_data[, c("IC2", "IC3", "IC4",
+                       "IC5", "IC6", "IC7", "IC8", "IC9",
+                       "IC10")] <- 
+        lapply(current_data[, c("IC2", "IC3", "IC4",
+                                "IC5", "IC6", "IC7", "IC8", "IC9",
+                                "IC10")], function(x) 
+                                  factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
+      imported_data(current_data)
+    } else if (!is.null(imported_data()) && iC10_edit() == "Preset") {
+      # Update imported_data()
+      current_data[, c("IC2", "IC3", "IC4",
+                       "IC5", "IC6", "IC7", "IC8", "IC9",
+                       "IC10")] <- 
+        lapply(current_data[, c("IC2", "IC3", "IC4",
+                                "IC5", "IC6", "IC7", "IC8", "IC9",
+                                "IC10")], function(x) 
+                                  factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
       imported_data(current_data)
     }
   })
@@ -1933,24 +1989,29 @@ server <- function(input, output, session) {
   # Mammaprint
   mammaprint_edit = reactive({ input$breast_cancer_new_prediction_mammaprint_annotation })
   observeEvent(mammaprint_edit(), {
+    # Create intermediate variable
+    current_data = imported_data()
     if (!is.null(imported_data()) && mammaprint_edit() != "Preset") {
       if (mammaprint_edit() == "Random") {
-        mammaprints = c("At risk", "No risk")
-        new_mammaprint = mammaprints[sample(seq(1, 2), 1)]
-        mammaprint_edit(new_mammaprint)
-      }
-      
-      # Create intermediate variable
-      current_data = imported_data()
-      
-      # Edit data
-      if (mammaprint_edit() == "At risk") {
+        mammaprints = c(`At risk` = 1, `No risk` = 0)
+        current_data[, "At risk"] = mammaprints[[sample(seq(1, 2), 1)]]
+      } else if (mammaprint_edit() == "At risk") {
         current_data[, "Mammaprint_risk_yes"] = 1
       } else {
         current_data[, "Mammaprint_risk_yes"] = 0
       }
-      
       # Update imported_data()
+      current_data[, "Mammaprint_risk_yes"] <- 
+        lapply(current_data[, "Mammaprint_risk_yes"], function(x) 
+          factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
+      imported_data(current_data)
+    } else if (!is.null(imported_data()) && mammaprint_edit() == "Preset") {
+      # Update imported_data()
+      current_data[, "Mammaprint_risk_yes"] <- 
+        lapply(current_data[, "Mammaprint_risk_yes"], function(x) 
+          factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
       imported_data(current_data)
     }
   })
@@ -1958,26 +2019,31 @@ server <- function(input, output, session) {
   # rorS
   rorS_edit = reactive({ input$breast_cancer_new_prediction_rors_annotation })
   observeEvent(rorS_edit(), {
+    # Create intermediate variable
+    current_data = imported_data()
     if (!is.null(imported_data()) && rorS_edit() != "Preset") {
       if (rorS_edit() == "Random") {
-        rorSs = c("High", "Intermediate", "Low")
-        new_rorS = rorSs[sample(seq(1, 3), 1)]
-        rorS_edit(new_rorS)
-      }
-      
-      # Create intermediate variable
-      current_data = imported_data()
-      
-      # Edit data
-      if (rorS_edit() == "High") {
+        rorSs = c(High= c(0, 1), Intermediate = c(1, 0), Low = c(0, 0))
+        current_data[, c("rorS_risk_interm", "rorS_risk_high")] = rorSs[[sample(seq(1, 3), 1)]]
+      } else if (rorS_edit() == "High") {
         current_data[, c("rorS_risk_interm", "rorS_risk_high")] = c(0, 1)
       } else if (rorS_edit() == "Intermediate") {
         current_data[, c("rorS_risk_interm", "rorS_risk_high")] = c(1, 0)
       } else if (rorS_edit() == "Low") {
         current_data[, c("rorS_risk_interm", "rorS_risk_high")] = c(0, 0)
       }
-      
       # Update imported_data()
+      current_data[, c("rorS_risk_interm", "rorS_risk_high")] <- 
+        lapply(current_data[, c("rorS_risk_interm", "rorS_risk_high")], function(x) 
+          factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
+      imported_data(current_data)
+    } else if (!is.null(imported_data()) && rorS_edit() == "Preset") {
+      # Update imported_data()
+      current_data[, c("rorS_risk_interm", "rorS_risk_high")] <- 
+        lapply(current_data[, c("rorS_risk_interm", "rorS_risk_high")], function(x) 
+          factor(x, levels = c(0, 1), labels = c(0, 1))
+        )
       imported_data(current_data)
     }
   })
@@ -2160,6 +2226,19 @@ server <- function(input, output, session) {
   
   # Make the prediction (+/- produce ROC plot) ###
   predict_new_data <- reactive({
+    # print(paste("Colnames of object:", paste(colnames(imported_data()), collapse = ", ")))
+    current_data = imported_data()
+    current_data[, c("HER2", "LumA", "LumB", "Normal", "T2", "Endo", "IC2", "IC3", "IC4",
+                     "IC5", "IC6", "IC7", "IC8", "IC9",
+                     "IC10", "Mammaprint_risk_yes", "rorS_risk_interm", "rorS_risk_high",
+                     "ER_hp", "ER_lp", "HER2_scmod1")] <- 
+      lapply(current_data[, c("HER2", "LumA", "LumB", "Normal", "T2", "Endo", "IC2", "IC3", "IC4",
+                              "IC5", "IC6", "IC7", "IC8", "IC9",
+                              "IC10", "Mammaprint_risk_yes", "rorS_risk_interm", "rorS_risk_high",
+                              "ER_hp", "ER_lp", "HER2_scmod1")], function(x) 
+                                factor(x, levels = c(0, 1), labels = c(0, 1))
+      )
+    imported_data(current_data)
     # Unique sample predictions
     if (import_data_type() %in% c("Random sample",
                                   "Import unique sample (genes only)",
@@ -2169,23 +2248,29 @@ server <- function(input, output, session) {
                            imported_data(), 
                            type = "prob")
       resp_prob = paste0(round(100*as.numeric(prediction["Responder"]), 2), "%")
+      print(resp_prob)
       non_resp_prob = paste0(round(100*as.numeric(prediction["Non_responder"]), 2), "%")
       
-      # Downloadable content
-      newdata = cbind(imported_data(), cbind(resp_prob, non_resp_prob))
-      names(newdata[, c(ncol(newdata)-1, ncol(newdata))]) = c("Response chance %", "No response chance %")
+      # Create a new data frame for the new variables
+      new_vars <- data.frame(
+        Responder = as.numeric(prediction[, "Responder"]),
+        Non_responder = as.numeric(prediction[, "Non_responder"]),
+        `Response chance %` = resp_prob,
+        `No response chance %` = non_resp_prob
+      )
+      
+      # Bind the new variables to your original data frame
+      newdata <- cbind(imported_data(), new_vars)
+      
+      # Update the Response column
+      newdata$Response <- factor(newdata$Response, levels = c("Responder", "Non_responder"),
+                                 labels = c("Responder", "Non_responder"))
       
       chosen_treatment = ifelse(imported_data()[, "Endo"] == 1, "endocrine treatment",
                                 "chemotherapy")
-      showModal(modalDialog(
-        title = "Result",
-        paste0("The patient has a ", resp_prob, " chance of responding to the chosen
-              treatment type (", chosen_treatment, ")."),
-        downloadButton('download_new_prediction_results', 'Download results'),
-        easyClose = TRUE
-      ))
       
-      return(list(predictions = newdata, plot = NULL, auc = NULL))
+      return(list(predictions = newdata, resp_prob = resp_prob, plot = NULL, auc = NULL, 
+                  chosen_treatment = chosen_treatment))
 
     } else if (import_data_type() == "Import pre-annotated dataset") {
       
@@ -2224,12 +2309,6 @@ server <- function(input, output, session) {
         
         return(list(predictions = newdata, plot = p, auc = auc_value))
       } else {
-        showModal(modalDialog(
-          title = "Result",
-          "No ROC plot could be generated.",
-          downloadButton('download_new_prediction_results', 'Download results'),
-          easyClose = TRUE
-        ))
         return(list(predictions = newdata, plot = NULL, auc = NULL))
       }
     }
@@ -2239,13 +2318,33 @@ server <- function(input, output, session) {
   output$newpred_ROC_plot <- renderPlot({
     input$predict_new_prediction_breast_cancer
     isolate({
-      p = predict_new_data()$plot
-      auc_val = predict_new_data()$auc
-      
-      p
-      # Add legend
-      legend(0.5, 0.25, legend=paste0("AUC = ", auc_val),
-             col="#2A5674", lty=1, cex=0.8)
+      if (import_data_type() != "Import pre-annotated dataset") {
+        showModal(modalDialog(
+          title = "Result",
+          paste0("The patient has a ", predict_new_data()$resp_prob, " chance of responding to the chosen
+              treatment type (", predict_new_data()$chosen_treatment, ")."),
+          downloadButton('download_new_prediction_results', 'Download results'),
+          easyClose = TRUE
+        ))
+      } else {
+        p = predict_new_data()$plot
+        if (is.null(p)) {
+          showModal(modalDialog(
+            title = "Result",
+            "No ROC plot could be generated.",
+            downloadButton('download_new_prediction_results', 'Download results'),
+            easyClose = TRUE
+          ))
+        } else {
+          auc_val = predict_new_data()$auc
+          
+          p
+          # Add legend
+          legend(0.5, 0.25, legend=paste0("AUC = ", auc_val),
+                 col="#2A5674", lty=1, cex=0.8)
+          
+        }
+      }
     })
   })
   
